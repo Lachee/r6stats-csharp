7700ff2f5013cc2245d877736683f62caf9efb51
Lachee <lachlan.h@hotmail.com>
2020-01-30 19:43:23 +1100

    Listing files

--- build.ps1
+++ build.ps1
@@ -10,3 +10,4 @@ dotnet pack ./R6Stats/R6Stats.csproj -c Release
 
 Write-Host "Collecting"
 Copy-Item -Path "R6Stats/bin/Release/*" -Recurse -Destination "Artifacts/" -Force
+Get-ChildItem -Path "Artifacts"
